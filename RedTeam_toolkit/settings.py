831947b59a662d0a32b84d105b6ea2833caa32b1
signorrayan <signor.rayan@gmail.com>
2022-05-10 03:55:52 +0430

    Update Workflow

--- RedTeam_toolkit/settings.py
+++ RedTeam_toolkit/settings.py
@@ -94,12 +94,12 @@ DATABASES = {
 if os.environ.get('GITHUB_WORKFLOW'):
     DATABASES = {
         'default': {
-           'ENGINE': 'django.db.backends.postgresql',
-           'NAME': 'github_actions',
-           'USER': 'postgres',
-           'PASSWORD': 'postgres',
-           'HOST': '127.0.0.1',
-           'PORT': '5432',
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'github_actions',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
         }
     }
 
