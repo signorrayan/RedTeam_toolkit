cf63369dc64ef08d17ef3e3d840269bf547ffb9c
signorrayan <signor.rayan@gmail.com>
2022-05-10 01:19:55 +0430

    Remove unused lines

--- toolkit/scripts/webapp/gather_url.py
+++ toolkit/scripts/webapp/gather_url.py
@@ -4,10 +4,20 @@ import subprocess
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# def get(target_url):
+#    pre_output = subprocess.getoutput(
+#        f"echo {target_url} | {BASE_DIR}/webapp/gau -t 15 -u"
+#    )
+#    if pre_output != "":
+#        result = pre_output.split("\n")
+#        return result
+#    else:
+#        return None
+
 
 def get(target_url):
     pre_output = subprocess.getoutput(
-        f"echo {target_url} | {BASE_DIR}/webapp/hakrawler -t 15 -u"
+        f"{BASE_DIR}/webapp/gau --mc 200 --blacklist ttf,woff,svg,png,jpg,gif,pdf,css,jpeg --threads 5 {target_url}"
     )
     if pre_output != "":
         result = pre_output.split("\n")
